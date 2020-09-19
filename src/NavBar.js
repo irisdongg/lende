5d3fec22afcb28602c2e4538d487bcca78263751
Kenny Chi <kennychi@berkeley.edu>
2020-09-19 14:17:22 -0700

    navbar.css changees

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Link} from "react-router-dom"
+import {BrowserRouter as Router, Link, Route} from "react-router-dom"
 import "./NavBar.css"
 import ReactSearchBox from 'react-search-box'
 
